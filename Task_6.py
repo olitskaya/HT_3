cc1ffd0ef06aa37ba035fc3c8a83c32982f8ab2f
olitskaya <94138631+olitskaya@users.noreply.github.com>
2021-11-25 15:44:21 +0200

    Create Task_6.py

---
+++ Task_6.py
@@ -0,0 +1,37 @@
+# Маємо рядок -->
+# "f98neroi4nr0c3n30irn03ien3c0rfekdno400wenwkowe00
+# koijn35pijnp46ij7k5j78p3kj546p465jnpoj35po6j345" ->
+# просто потицяв по клаві.
+# Створіть ф-цію, яка буде отримувати рядки на зразок цього,
+# яка оброблює наступні випадки:
+# - якщо довжина рядка в діапазоні 30-50 ->
+# прінтує довжину, кількість букв та цифр
+# - якщо довжина менше 30 ->
+# прінтує суму всіх чисел та окремо рядок без цифр (лише з буквами)
+# - якщо довжина більше 50 -> ваша фантазія
+
+def perevirka(a):
+    dovzhyna = len(a)
+    if 29 < dovzhyna < 51 :
+        print('Довжина рядка: ', len(a))
+        num = [int(i) for i in a if i.isdigit()]
+        print('Кількість цифр:', len(num))
+        print('Кількість букв: ', len(a) - len(num))
+    if dovzhyna < 30:
+        sum = 0
+        for x in a:
+            if x.isdigit() == True:
+                z = int(x)
+                sum = sum + z
+        print('Сума всіх чисел: ', sum)
+        b = ''  
+        for c in a:  
+            if c not in ('0','1','2','3','4','5','6','7','8','9'):  
+                b = b + c
+        print('Рядок без цифр: ', b)        
+    if dovzhyna > 50 :
+        print('Гарного дня!')
+    return
+
+ryadok = str(input('Введіть рядок: '))
+perevirka(ryadok)
